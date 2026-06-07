e692d582f5bf170ee0f29caa850d860055b22721
Richu232 <richamahajan363@gmail.com>
2026-06-08 01:30:17 +0530

    Enhance admin interface with Question and Choice models
    Added Question and Choice models to admin with custom representations. Updated CourseAdmin to include QuestionInline.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,38 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+# Seven imported classes required (Course, Lesson, Instructor, Learner, Question, Choice, Submission)
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Choice Inline allows adding choices right inside the Question view
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
 
+# Question Inline allows editing questions directly inside the Course page view
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
+# Custom Admin representation for Question
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'course', 'grade']
 
+# Custom Admin representation for Lesson
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'order']
 
-# Register your models here.
+# Custom Admin representation for Course (includes items and lessons inline view)
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
-
-# <HINT> Register Question and Choice models here
-
+# Registering models on Django Site Portal
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
 admin.site.register(Instructor)
 admin.site.register(Learner)
