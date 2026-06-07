32e343467fc501cdd8a7499fc082587a60d1a5fb
Richu232 <richamahajan363@gmail.com>
2026-06-08 01:27:44 +0530

    Introduce Question, Choice, and Submission models
    Added new models for questions, choices, and submissions to enhance course functionality.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -7,97 +7,49 @@ except Exception:
     sys.exit()
 
 from django.conf import settings
-import uuid
 
+# Existing models like Course, Lesson, Enrollment, etc. remain unchanged here...
 
-# Instructor model
-class Instructor(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-    full_time = models.BooleanField(default=True)
-    total_learners = models.IntegerField()
+# --- NEW MODELS FOR TASK 1 ---
 
-    def __str__(self):
-        return self.user.username
-
-
-# Learner model
-class Learner(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-    STUDENT = 'student'
-    DEVELOPER = 'developer'
-    DATA_SCIENTIST = 'data_scientist'
-    DATABASE_ADMIN = 'dba'
-    OCCUPATION_CHOICES = [
-        (STUDENT, 'Student'),
-        (DEVELOPER, 'Developer'),
-        (DATA_SCIENTIST, 'Data Scientist'),
-        (DATABASE_ADMIN, 'Database Admin')
-    ]
-    occupation = models.CharField(
-        null=False,
-        max_length=20,
-        choices=OCCUPATION_CHOICES,
-        default=STUDENT
-    )
-    social_link = models.URLField(max_length=200)
-
-    def __str__(self):
-        return self.user.username + "," + \
-               self.occupation
-
-
-# Course model
-class Course(models.Model):
-    name = models.CharField(null=False, max_length=30, default='online course')
-    image = models.ImageField(upload_to='course_images/')
-    description = models.CharField(max_length=1000)
-    pub_date = models.DateField(null=True)
-    instructors = models.ManyToManyField(Instructor)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
-    total_enrollment = models.IntegerField(default=0)
-    is_enrolled = False
-
-    def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
-
-
-# Lesson model
-class Lesson(models.Model):
-    title = models.CharField(max_length=200, default="title")
-    order = models.IntegerField(default=0)
+class Question(models.Model):
+    # Foreign key link to Course (Many-to-One)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    content = models.TextField()
+    # Question text text field
+    question_text = models.CharField(max_length=500)
+    # Grade point value for the question
+    grade = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.question_text
+
+    # Provided method to calculate if the learner answers correctly
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 
-# Enrollment model
-# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
-# And we could use the enrollment to track information such as exam submissions
-class Enrollment(models.Model):
-    AUDIT = 'audit'
-    HONOR = 'honor'
-    BETA = 'BETA'
-    COURSE_MODES = [
-        (AUDIT, 'Audit'),
-        (HONOR, 'Honor'),
-        (BETA, 'BETA')
-    ]
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    date_enrolled = models.DateField(default=now)
-    mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
-    rating = models.FloatField(default=5.0)
+class Choice(models.Model):
+    # Foreign key link to Question (Many-to-One)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # Choice content text field
+    choice_text = models.CharField(max_length=500)
+    # Boolean flag determining if the choice is a correct answer
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
 
 
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    # Foreign key link to Enrollment (Many-to-One)
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    # Many-to-Many relationship with Choice model
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return "Submission " + str(self.id) + " for " + self.enrollment.user.username
